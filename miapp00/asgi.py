3622d72a3466858fd53efc16c80b93e36d6aca11
bryangj23 <brayanguardodiaz0@gmail.com>
2021-07-27 04:50:03 -0500

    Iniciando proyecto Django

---
+++ miapp00/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI miapp00 for miapp00 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miapp00.settings')
+
+application = get_asgi_application()
